6cf9c191baf97e6a72cfa8f653b4d836b6d7734f
asiahhh <125533782+asiahhh@users.noreply.github.com>
2023-03-19 12:59:27 +0000

    added img

--- main.py
+++ main.py
@@ -2,26 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  with open('content.txt', 'r') as file: # read only
-    intro = file.read()  
-  data = None
-  if request.method =='POST':
-    data = request.form['data']
-    data = data # change data
-    with open('data.txt', 'a') as file: # add or create file
-      file.write(data + '\n')
-  return render_template('index.html', data=data, intro=intro)
-  if request.method == 'POST':
-    data = request.form['data']
-    if data < 100:
-      return render_template('less.html')
-    elif 100 <= data < 1440:
-      return render_template('more.html')
-    else:
-      return render_template('middle.html')
-
+    with open('content.txt', 'r') as file:
+        intro = file.read()
+    data = None
+    if request.method == 'POST':
+        data = request.form['data']
+        data = int(data) # convert data to integer
+        if data < 100:
+            return render_template('less.html')
+        elif 100 <= data < 1440:
+            return render_template('middle.html')
+        else:
+            return render_template('more.html')
+    return render_template('index.html', data=data, intro=intro)
 
 app.run(host='0.0.0.0', port=81)
