17f752a271c701d338bfea92f6f9d19d1331fe08
asiahhh <125533782+asiahhh@users.noreply.github.com>
2023-03-19 09:01:57 +0000

    added templates

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    input = request.form['data']
-    if int(input) > 100:
-      return render_template('good_job.html')
-    else:
-      return render_template('a_little_bit_more.html')
   with open('content.txt', 'r') as file: # read only
     intro = file.read()  
   data = None
@@ -20,6 +14,14 @@ def index():
     with open('data.txt', 'a') as file: # add or create file
       file.write(data + '\n')
   return render_template('index.html', data=data, intro=intro)
+  if request.method == 'POST':
+    data = request.form['data']
+    if data < 100:
+      return render_template('less.html')
+    elif 100 <= data < 1440:
+      return render_template('more.html')
+    else:
+      return render_template('middle.html')
 
 
 app.run(host='0.0.0.0', port=81)
